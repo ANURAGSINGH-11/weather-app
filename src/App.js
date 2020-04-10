6360aad1629d7f4d427015cf23060b9743b4e975
ANURAGSINGH-11 <anuragsingh.cs.it@gmail.com>
2020-04-10 13:37:00 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import React from "react";
 import "./App.css";
 import Form from "./app_component/form.component";
 import Weather from "./app_component/weather.component";
@@ -7,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // git project https://github.com/erikflowers/weather-icons
 import "weather-icons/css/weather-icons.css";
 
-const Api_Key = "429736441cf3572838aa10530929f7cd";
+const Api_Key = "0b9513fe6a317ddb98436e8b36b77d97";
 
 class App extends React.Component {
   constructor() {
@@ -76,7 +77,7 @@ class App extends React.Component {
 
     if (country && city) {
       const api_call = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
       );
 
       const response = await api_call.json();
